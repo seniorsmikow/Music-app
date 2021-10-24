df86aaa1d7daf7f1cd88de0d0205d0c697c8b9e3
Ilya <senior.smikow@yandex.ru>
2021-10-24 18:34:32 +0300

    change main page and header

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,40 +1,30 @@
-import React from 'react'
 import { GhostAnimation } from '../../components/GhostAnimation/GhostAnimation'
 import styles from './HomePage.module.scss'
-import network from '../../img/smm-prodvizhenie.png'
-import { Slider } from '../../components/Slider/Slider'
+import Button from '@mui/material/Button'
+import { useHistory } from 'react-router'
 
 
 export const HomePage = () => {
+
+    const history = useHistory()
+
+    const toNewReleases = () => {
+        history.push('/new_releases')
+    }
+
     return (
         <div className={styles.home__page_root}>
-            <Slider />
             <div className={styles.home__animation_block}>
                 <GhostAnimation /> 
             </div>
-            <div className={styles.home__title}>
-                <h1>"В разработке"</h1>
-                <h3>Социальная сеть для разработчиков</h3>
-            </div>
-            <div className={styles.home__main}>
-                <div className={styles.home__schema_img}>
-                    <img src={network} alt="social network"/>
+            <div className={styles.home__page_text}>
+                <h1>Не знаете, что послушать?</h1>
+                <div>
+                    Начните с лучших новинок.
                 </div>
-                <p>
-
-                    Этот проект создан на базе API 
-                    <a href="https://social-network.samuraijs.com/docs">"SamuraiJS Social Network"</a>.
-                    В далёком 2015 году разработчик из Беларуси создаёт youtube канал
-                    <a href="https://www.youtube.com/c/ITKAMASUTRA/featured">IT-KAMASUTRA </a>,
-                    где учит людей программированию.
-                    <span>
-                        Канал про программирование на JavaScript. Секреты и инсайды, как стать программистом абсолютно любому человеку. 
-                    </span>
-                </p>
             </div>
-            <div className={styles.home__youtube_video}>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/RKrkQxFYJ1c" title="YouTube video player"  
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+            <div className={styles.home__button_news}>
+                <Button variant="contained" onClick={() => toNewReleases()}>Перейти к новинкам</Button>
             </div>
         </div>
     )
